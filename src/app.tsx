00a8d26de28a5eecc2acdb762a7b7d87f04fbf43
Mustafa Ihssan <mustafa.ihssan@gmail.com>
2025-08-15 14:58:15 +0300

    install shadcn

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import './index.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
   return (
     <>
       <div>
         <h1 className="text-3xl font-bold underline">Hello World</h1>
+        <Button>Click me</Button>
       </div>
     </>
   )
